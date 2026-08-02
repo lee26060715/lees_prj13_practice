398d6ba442785bd0809656c12dbb8c593ab4149e
DESKTOP-8P1B1NH\user1 <leesenghon@gmail.com>
2026-08-03 08:56:45 +0900

    프로그램 개선

--- service/service.py
+++ service/service.py
@@ -21,27 +21,51 @@ def plus_music():
 
 # 플레이 리스트 보기
 def play_music_list():
+    if not music_play_list:
+        print("등록된 음악이 없습니다.")
+        return
     print("\n----- 플레이 리스트 -----")
     print("번호 | 제목")
     for idx, m in enumerate(music_play_list):
         print(f"{idx + 1}   | {m.title}")
-    if not music_play_list:
-        print("등록된 음악이 없습니다.")
-        return
+
 
 
 # 플레이 리스트 상세보기
 def view_detail_play_music_list():
-    print("--- 플레이 리스트 상세보기 ---")
-    music_num = int(input("상세보기할 음악 번호 입력: "))
-    m = music_play_list[music_num - 1]
+    if not music_play_list:
+        print("등록된 음악이 없습니다.")
+        return
 
-    print(m)
+    try:
+        print("--- 플레이 리스트 상세보기 ---")
+        music_num = int(input("상세보기할 음악 번호 입력: "))
+        m = music_play_list[music_num - 1]
+        print(m)
+
+    except (ValueError, IndexError):
+        print("올바른 번호를 입력해주세요.")
 
 
 # 플레이리스트 삭제하기
 def delete_play_music_list():
-    print("\n---플레이 리스트에서 삭제---")
-    music_num = int(input("플레이리스트에서 삭제할 음악 넘버: "))
-    del music_play_list[music_num - 1]
-    print("플레이 리스트에서 삭제되었습니다.\n")
+    while True:
+        if not music_play_list:
+            print("등록된 음악이 없습니다.")
+            return
+
+        try:
+            print("\n--- 플레이 리스트에서 삭제 ---")
+            music_num = int(input("플레이리스트에서 삭제할 음악 번호: "))
+
+            del music_play_list[music_num - 1]
+            print("플레이 리스트에서 삭제되었습니다.\n")
+
+            answer = input("음악을 더 삭제하시겠습니까? (y/n): ")
+
+            if answer != "y":
+                print("\n플레이리스트 삭제를 종료합니다.")
+                break
+
+        except (ValueError, IndexError):
+            print("올바른 번호를 입력해주세요.")
